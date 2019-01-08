d1d3eff5144f5213e5e3b368ef341a218ecadc62
MCYouks <andreas.blondeau@gmail.com>
2019-01-08 18:19:11 +0100

    Added second part with audio directory

--- keywordDetector.py
+++ keywordDetector.py
@@ -9,8 +9,9 @@ keywords = ('carte', 'cartes', 'nouvelle carte', 'nouvelles cartes',
             'ticket', 'tickets', 'nouveau ticket', 'nouveaux tickets')
 
 # Directories
-FROM_DIRECTORY = "FROM_DIRECTORY"
-TO_DIRECTORY = "TO_DIRECTORY"
+FROM_DIRECTORY_JSON = "FROM_DIRECTORY_JSON"
+FROM_DIRECTORY_AUDIO = "FROM_DIRECTORY_AUDIO"
+TO_DIRECTORY_AUDIO = "TO_DIRECTORY_AUDIO"
 
 
 def detect_keywords(file_name):
@@ -18,7 +19,7 @@ def detect_keywords(file_name):
 
     file_path = os.path.join(
                     os.path.dirname(__file__),
-                    FROM_DIRECTORY,
+                    FROM_DIRECTORY_JSON,
                     file_name)
 
     # Open the json file
@@ -47,18 +48,18 @@ def detect_keywords(file_name):
 
 
 def transfer_file(file_name):
-    """ Transfere a json file from FROM_DIRECTORY to TO_DIRECTORY """
+    """ Transfere a json file from FROM_DIRECTORY_AUDIO to TO_DIRECTORY_AUDIO """
     
     # Source file
     src = os.path.join(
             os.path.dirname(__file__),
-            FROM_DIRECTORY,
+            FROM_DIRECTORY_AUDIO,
             file_name)
 
     # Destination file
     dst = os.path.join(
             os.path.dirname(__file__),
-            TO_DIRECTORY,
+            TO_DIRECTORY_AUDIO,
             file_name)
 
     # Copy file from source to destination
@@ -72,10 +73,10 @@ def transfer_file(file_name):
 def newFileScanner():
     """ Detects when new file in the directory. """
 
-    before = dict ([(f, None) for f in os.listdir(FROM_DIRECTORY)])
+    before = dict ([(f, None) for f in os.listdir(FROM_DIRECTORY_JSON)])
     while 1:
         time.sleep (10)
-        after = dict ([(f, None) for f in os.listdir(FROM_DIRECTORY)])
+        after = dict ([(f, None) for f in os.listdir(FROM_DIRECTORY_JSON)])
         added = [f for f in after if not f in before]
         removed = [f for f in before if not f in after]
 
